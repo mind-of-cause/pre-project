f72d5e14e18a473fc8715aab2d557e7cbe895490
mind_of_cause <57660302+mind-of-cause@users.noreply.github.com>
2024-03-29 16:22:32 +0300

    Add files via upload

--- Lugovkin-K-PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Egg6.java
+++ Lugovkin-K-PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Egg6.java
@@ -6,9 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Egg6 {
     private final Needle7 needle;
+
     @Autowired
-    public Egg6(Needle7 needle) {this.needle = needle;}
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
-    public String toString() { return ", в яйце иголка " + needle.toString(); }
+    public String toString() {
+        return ", в яйце иголка " + needle.toString();
+    }
 }
--- Lugovkin-K-PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Needle7.java
+++ Lugovkin-K-PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Needle7.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-    private final Deth8 deth;
-    public Needle7(Deth8 deth) {this.deth = deth;}
+    private Deth8 deth;
+
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + deth;
+        return ", смерть Кощея на игле :( ";
     }
 }
