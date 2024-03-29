0fdf65bca4dbda9692dca4ac5b43cf3f5e2c2dd6
mind_of_cause <57660302+mind-of-cause@users.noreply.github.com>
2024-03-29 10:58:51 +0300

    Add files via upload

---
+++ Lugovkin-K-PP_2_1_3_SpringBeans/Lugovkin-K-PP_2_1_3_SpringBeans/src/main/java/app/model/Timer.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+@Component ("Timer")
+public class Timer {
+
+    private final Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
